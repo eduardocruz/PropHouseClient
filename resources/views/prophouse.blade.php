25a27741e48a791b264dd9be0b1d7196950a6cef
Eduardo Cruz <eduardo@eduardocruz.com>
2022-10-21 20:40:57 -0300

    Link to open auctions and communities in the main page

--- resources/views/prophouse.blade.php
+++ resources/views/prophouse.blade.php
@@ -123,14 +123,18 @@
 
                     <div class="mx-auto mt-5 max-w-md sm:flex sm:justify-center md:mt-8">
 
-                        {{--
+
                         <div class="rounded-md shadow">
-                            <a href="#" class="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 md:py-4 md:px-10 md:text-lg">Get started</a>
+                            <a href="/auctions" class="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 md:py-4 md:px-10 md:text-lg">
+                                Open Auctions
+                            </a>
                         </div>
                         <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
-                            <a href="#" class="flex w-full items-center justify-center rounded-md border border-transparent bg-white px-8 py-3 text-base font-medium text-indigo-600 hover:bg-gray-50 md:py-4 md:px-10 md:text-lg">Live demo</a>
+                            <a href="/communities" class="flex w-full items-center justify-center rounded-md border border-transparent bg-white px-8 py-3 text-base font-medium text-indigo-600 hover:bg-gray-50 md:py-4 md:px-10 md:text-lg">
+                                Communities
+                            </a>
                         </div>
-                        --}}
+
                     </div>
                 </div>
             </main>
