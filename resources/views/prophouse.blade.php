7d63d3dbe6124da96cd3ccd455194231b73bb381
Eduardo Cruz <eduardo@eduardocruz.com>
2022-10-21 14:55:28 -0300

    Adding noggles to landing page

--- resources/views/prophouse.blade.php
+++ resources/views/prophouse.blade.php
@@ -114,7 +114,7 @@
                 <div class="text-center">
                     <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
                         <span class="block xl:inline">Prop House</span>
-                        <span class="block text-indigo-600 xl:inline">Client</span>
+                        <span class="block text-indigo-600 xl:inline">Client ⌐◨-◨</span>
                     </h1>
                     <p class="mx-auto mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:max-w-3xl md:text-xl">
                         This is an MVP for a Nouns Prop House proposal. The goal is to use the new Prop House graphQL to
