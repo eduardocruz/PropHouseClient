606427322bda153c9801c78834dddedd7294fabc
Eduardo Cruz <eduardo@eduardocruz.com>
2022-10-21 14:48:48 -0300

    Removing unused buttons while features are not added

--- resources/views/prophouse.blade.php
+++ resources/views/prophouse.blade.php
@@ -120,13 +120,17 @@
                         This is an MVP for a Nouns Prop House proposal. The goal is to use the new Prop House graphQL to
                         extendo the features and usability already provided by Prop House.
                     </p>
+
                     <div class="mx-auto mt-5 max-w-md sm:flex sm:justify-center md:mt-8">
+
+                        {{--
                         <div class="rounded-md shadow">
                             <a href="#" class="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 md:py-4 md:px-10 md:text-lg">Get started</a>
                         </div>
                         <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
                             <a href="#" class="flex w-full items-center justify-center rounded-md border border-transparent bg-white px-8 py-3 text-base font-medium text-indigo-600 hover:bg-gray-50 md:py-4 md:px-10 md:text-lg">Live demo</a>
                         </div>
+                        --}}
                     </div>
                 </div>
             </main>
