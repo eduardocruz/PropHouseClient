e4c0e61220416dfe5f22627fd98c10a109200ff4
Eduardo Cruz <eduardo@eduardocruz.com>
2022-10-23 17:46:04 -0300

    Adding upcoming auctions

--- app/Http/Controllers/AuctionController.php
+++ app/Http/Controllers/AuctionController.php
@@ -10,7 +10,6 @@ use Illuminate\Http\Request;
 class AuctionController extends Controller
 {
 
-
     public function open()
     {
         $client = new \GraphQL\Client('https://prod.backend.prop.house/graphql');
@@ -53,6 +52,50 @@ class AuctionController extends Controller
         $data = $response->getData();
         $auctions = collect($data->auctionsByStatus);
         return view('auctions.open', compact('auctions'));
+    }
+
+    public function upcoming()
+    {
+        $client = new \GraphQL\Client('https://prod.backend.prop.house/graphql');
+
+        $gql = (new \GraphQL\Query('auctionsByStatus'))
+            ->setArguments(['status' => new RawObject('Upcoming')])
+            ->setSelectionSet(
+                [
+                    'id',
+                    'title',
+                    'status',
+                    'startTime',
+                    'proposalEndTime',
+                    'fundingAmount',
+                    'currencyType',
+                    'description',
+                    'numWinners',
+                    (new Query('community'))
+                        ->setSelectionSet(
+                            [
+                                'name',
+                                'profileImageUrl',
+                                'description'
+                            ]
+                        ),
+                    (new Query('proposals'))
+                        ->setSelectionSet(
+                            [
+                                'id',
+                                'title',
+                                'what',
+                                'voteCount',
+                                'createdDate'
+                            ]
+                        )
+                ]
+            );
+
+        $response = $client->runQuery($gql);
+        $data = $response->getData();
+        $auctions = collect($data->auctionsByStatus);
+        return view('auctions.upcoming', compact('auctions'));
     }
 
     public function home()
