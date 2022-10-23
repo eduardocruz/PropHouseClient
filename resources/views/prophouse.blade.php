f307e10100214c5fdb4b125475631f26978794cc
Eduardo Cruz <eduardo@eduardocruz.com>
2022-10-23 15:30:39 -0300

    Link to auction page

--- resources/views/prophouse.blade.php
+++ resources/views/prophouse.blade.php
@@ -140,6 +140,8 @@
                             @foreach($auctions as $auction)
                                 <li class="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow">
                                     <div class="flex w-full items-center justify-between space-x-6 p-6">
+                                        <a href="/auctions/{{$auction->id}}" class="relative -mr-px inline-flex w-0 flex-1 items-center justify-center rounded-bl-lg border border-transparent py-4 text-sm font-medium text-gray-700 hover:text-gray-500">
+
                                         <div class="flex-1 truncate">
                                             <div class="flex items-center space-x-3">
                                                 <h3 class="truncate text-sm font-medium text-gray-900">{{$auction->title}}</h3>
@@ -150,11 +152,12 @@
                                         <img class="h-10 w-10 flex-shrink-0 rounded-full bg-gray-300"
                                              src="{{$auction->community->profileImageUrl}}"
                                              alt="">
+                                        </a>
                                     </div>
                                     <div>
                                         <div class="-mt-px flex divide-x divide-gray-200">
                                             <div class="flex w-0 flex-1">
-                                                <a href="#" class="relative -mr-px inline-flex w-0 flex-1 items-center justify-center rounded-bl-lg border border-transparent py-4 text-sm font-medium text-gray-700 hover:text-gray-500">
+                                                <a href="/auctions/{{$auction->id}}" class="relative -mr-px inline-flex w-0 flex-1 items-center justify-center rounded-bl-lg border border-transparent py-4 text-sm font-medium text-gray-700 hover:text-gray-500">
                                                     <span class="ml-3">
                                                         Submission Deadline: <br/>
                                                         {{\Carbon\Carbon::parse($auction->proposalEndTime)->diffForHumans()}}
@@ -162,7 +165,7 @@
                                                 </a>
                                             </div>
                                             <div class="-ml-px flex w-0 flex-1">
-                                                <a href="#" class="relative inline-flex w-0 flex-1 items-center justify-center rounded-br-lg border border-transparent py-4 text-sm font-medium text-gray-700 hover:text-gray-500">
+                                                <a href="/auctions/{{$auction->id}}" class="relative inline-flex w-0 flex-1 items-center justify-center rounded-br-lg border border-transparent py-4 text-sm font-medium text-gray-700 hover:text-gray-500">
                                                     <span class="ml-3">
                                                         Funding: <br/>
                                                         {{$auction->numWinners}} x
